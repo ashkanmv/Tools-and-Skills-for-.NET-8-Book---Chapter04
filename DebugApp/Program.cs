97aac130dd2cc83b185f1d6634fa57a4a89d8847
a.mirzaei <ashkanmv@gmail.com>
2024-10-13 19:06:40 +0330

    Add project files.

---
+++ DebugApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using Northwind.EntityModels;
+using Packt.Shared;
+
+WebConfig config = new();
+config.OutputAll();
+Product product = new()
+{
+    ProductId = 1,
+    CategoryId = 1,
+    ProductName = "Chai",
+    UnitPrice = 1,
+    UnitsInStock = 1,
+    Discontinued = false
+};
+ WriteLine("Press enter to exit.");
